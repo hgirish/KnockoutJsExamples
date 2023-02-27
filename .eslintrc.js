20cde1470cb8745865502370254b8d6896a0203c
Girish <girish@post.com>
2023-02-26 17:41:24 -0800

    eslint linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,38 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "no-var": "error",
-        "prefer-const" : "error"
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'windows'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'no-var': 'error',
+    'prefer-const' : 'error'
 
-    },
-    ignorePatterns: ['src/test/*', 'lib/**'], // <<< ignore all files in test folder
-    "globals": {
-    "$": true,
-        "ko": true
-    }
+  },
+  ignorePatterns: ['src/test/*', 'lib/**'], // <<< ignore all files in test folder
+  'globals': {
+    '$': true,
+    'ko': true
+  }
 };
